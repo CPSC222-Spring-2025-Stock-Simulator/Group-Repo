56e7e8ee30ac56cb8c7c160e15c596a251033c63
Pinecone-Programs <flowhid@gmail.com>
2025-03-29 17:37:39 -0700

    Added back what was once lost

--- CPSC222_Final_Project_Alexii_Cory_Lizé/CPSC222_StockSim/src/StockGUI/StockGUI.java
+++ CPSC222_Final_Project_Alexii_Cory_Lizé/CPSC222_StockSim/src/StockGUI/StockGUI.java
@@ -1,7 +1,42 @@
 package StockGUI;
 
-public class StockGUI {
+import Backend.CLI;
+import Backend.Main;
 
-    public void updateGUI() {
+public class StockGUI {
+    CLI mainCLI = Main.getCli();
+
+
+    public void updateGUI(){}
+    //TODO: Refreshes the GUI
+
+
+    //TODO: Use these when triggering their respective buttons
+    private void pause(){
+        mainCLI.pause();
     }
+    private void play(){
+        mainCLI.play();
+    }
+    private void quit(){
+        mainCLI.quit();
+        //TODO: Close the GUI thread using its safe close thing (I don't remember how, sorry)
+    }
+
+    //TODO: Use these if you add the ability to enter
+    // values before the main program starts or just
+    // delete if we don't get around to that
+    private void setPeople(int numPeople){
+        mainCLI.setPeople(numPeople);
+    }
+    private void setMoney(int amount){
+        mainCLI.setStartingMoney(amount);
+    }
+    private void setCycleAmount(int amount){
+        mainCLI.setCycleCount(amount);
+    }
+    private void setCycleLength(double seconds){
+        mainCLI.setCycleLength(seconds);
+    }
+
 }
