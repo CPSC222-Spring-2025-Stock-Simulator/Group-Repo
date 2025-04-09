0933ee0efcb1c1fc6e4fa623f2dfa709bff8a68f
liz <lize6012@gmail.com>
2025-04-08 17:35:52 -0700

    updated the StockGUI

--- CPSC222_Final_Project_Alexii_Cory_Lizé/CPSC222_StockSim/src/Backend/Main.java
+++ CPSC222_Final_Project_Alexii_Cory_Lizé/CPSC222_StockSim/src/Backend/Main.java
@@ -12,19 +12,19 @@ import javafx.stage.Stage;
 public class Main extends Application
 {
     private static StockGUI stockGUI;
-    private static CLI cli;
+    //private static CLI cli;
     private static Random random;
 
     public static void main(String[] args) throws InterruptedException
     {
-
-        System.out.println("some thing random");
         Random random = new Random();
 
 
-        cli = new CLI(random) ;
+        /*cli = new CLI(random) ;
         Thread t = new Thread(cli);
         t.start();
+
+         */
 
         Thread javafxThread = new Thread(() -> Application.launch());
         javafxThread.start();
@@ -44,9 +44,9 @@ public class Main extends Application
         Backend.getStopWatch().start();
     }
 
-    public static CLI getCli() {
+    /*public static CLI getCli() {
         return cli;
-    }
+    }*/
 
     @Override
     public void start(Stage stage) throws Exception {
