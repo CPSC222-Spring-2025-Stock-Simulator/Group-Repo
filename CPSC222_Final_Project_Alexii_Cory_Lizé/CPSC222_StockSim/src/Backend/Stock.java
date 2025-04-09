77217e66576eb1a10e345c039ff57381b39233a3
CoryStecyk <corystecyk@gmail.com>
2025-04-09 15:44:43 -0700

    some stuff

--- CPSC222_Final_Project_Alexii_Cory_Lizé/CPSC222_StockSim/src/Backend/Stock.java
+++ CPSC222_Final_Project_Alexii_Cory_Lizé/CPSC222_StockSim/src/Backend/Stock.java
@@ -2,74 +2,98 @@ package Backend;
 
 import java.util.Random;
 
-public class Stock {
-    private float price;
-    private float velocity;
-    private float acceleration;
-    private Random rng = new Random();
+public class Stock
+{
+    private float price ;
+    private float velocity ;
+    private float acceleration ;
+    private Random rng = new Random() ;
+    private boolean isEventForced ;
 
-    public Stock(float startPrice) {
-        price = startPrice;
-        this.velocity = 0;
-        this.acceleration = 0;
+    public Stock(float startPrice)
+    {
+        price = startPrice ;
+        this.velocity = 0 ;
+        this.acceleration = 0 ;
+        this.isEventForced = false ;
     }
 
-    public float getPrice() {
+    public float getPrice()
+    {
         return price;
     }
 
-    public synchronized void updateVelocity(float deltaVelocity) {
-        this.velocity += deltaVelocity;
+    public synchronized void updateVelocity(float deltaVelocity)
+    {
+        this.velocity += deltaVelocity ;
     }
 
-    public synchronized void updateAcceleration(float deltaAcc) {
-        this.acceleration += deltaAcc;
+    public synchronized void updateAcceleration(float deltaAcc)
+    {
+        this.acceleration += deltaAcc ;
     }
 
     public synchronized void update() {
-        velocity += acceleration;
-        price += velocity;
+        velocity += acceleration ;
+        price += velocity ;
 
-        eventUpdate();
+        eventUpdate() ;
 
         // price*= .9f ;
-        velocity -= API.getStockStartPrice() * .1f;
+        velocity -= API.getStockStartPrice() * .1f ;
 
         if (price < 1)          // prevent price below 1
-            price = 1;
+            price = 1 ;
 
-        acceleration = 0;       // reset after each update              /TODO: see if we need this or not
+        acceleration = 0 ;       // reset after each update              /TODO: see if we need this or not
 
-        API.setCurrentStockPrice(price);
-        API.addNextStockPrice(price);
+        API.setCurrentStockPrice(price) ;
+        API.addNextStockPrice(price) ;
     }
 
-    public void eventUpdate() {
-        if (rng.nextFloat(0f, 1.0f) < API.getEventChance()) {
-            float event = rng.nextFloat(0, 2);
+    public void eventUpdate()
+    {
+        if (isEventForced)
+            forceEvent(// get event input from cli);
+            return ;
 
-            if (event > 1) {
-                API.setEventType("Good");
-                API.setEventStrength(event);
+        if (rng.nextFloat(0f, 1.0f) < API.getEventChance())
+        {
+            float event = rng.nextFloat(0, 2) ;
+
+            if (event > 1)
+            {
+                API.setEventType("Good") ;
+                API.setEventStrength(event) ;
             } else {
-                API.setEventType("Bad");
-                API.setEventStrength(2 - event);
+                API.setEventType("Bad") ;
+                API.setEventStrength(2 - event) ;
             }
 
-            price += API.getStockStartPrice() * event;                             // event affect price (short term)
-        } else API.setEventType(null);
+            price += API.getStockStartPrice() * event ;                             // event affect price (short term)
+        } else API.setEventType(null) ;
     }
 
-    public void forceEvent(float event) {
+    public void forceEvent(float event) throws InterruptedException
+    {
+        Thread.sleep((long) (API.getCycleLength()*1000));
+        privateForceEvent(event) ;
+    }
+
+    private void privateForceEvent(float event)
+    {
         // bad event is (0.0,1.0], good event is (1.0,2.0]
-        if (event > 1) {
-            API.setEventType("Good");
-            API.setEventStrength(event);
+        if (event > 1)
+        {
+            API.setEventType("Good") ;
+            API.setEventStrength(event) ;
         } else {
-            API.setEventType("Bad");
-            API.setEventStrength(2 - event);
+            API.setEventType("Bad" ) ;
+            API.setEventStrength(2 - event) ;
         }
 
-        price += API.getStockStartPrice() * event;                             // event affect price (short term)else API.setEventType(null);
+        price += API.getStockStartPrice() * event ;                             // event affect price (short term)else API.setEventType(null);
+
+        isEventForced = false ;
     }
 }
